f1db76519a0447799e090b3119b9628643768dc4
Eduardo <eduardo.petrilli@mag-it.com.br>
2022-05-13 20:12:49 -0300

    Adicionado calculadora de IMC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { FooterComponent } from './footer/footer.component';
+import { FirstViewComponent } from './first-view/first-view.component';
+import { HeaderComponent } from './header/header.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    FirstViewComponent,
+    HeaderComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CalculadoraService } from './services/calculadora.service';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.scss']
+})
+export class CalculadoraComponent implements OnInit {
+  idade = 25;
+  peso = 60;
+  altura = 170;
+  sexo = '';
+  result: string = '';
+  msg: string = '';
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  masculino(){
+    this.sexo = 'M';
+  }
+
+  femenino(){
+    this.sexo = 'F';
+  }
+
+  calcularIMC(){
+    const IMC = this.peso / Math.pow(this.altura/100,2);
+    if (IMC <= 18) {
+      this.result = 'Abaixo do peso';
+      this.msg = 'Você se encontra abaixo do peso normal';
+    } else if (IMC >= 18.01 && IMC <= 24.99) {
+      this.result = 'Peso normal';
+      this.msg = 'Seu peso está saudável';
+    } else if (IMC >= 25 && IMC <= 29.99) {
+      this.result = 'Sobrepeso';
+      this.msg = 'Acima do normal';
+    } else if (IMC >= 30 && IMC <= 34.99) {
+      this.result = 'Obesidade';
+      this.msg = 'Está em estado de obesidade';
+    } else if (IMC >= 35) {
+      this.result = 'Muito Obeso (Obesidade mórbida)';
+      this.msg = 'Seu peso está muito acima do normal';
+    }
+    this.result = IMC.toFixed(2);
+  }
+
+}
